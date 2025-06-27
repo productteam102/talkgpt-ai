0f1b8307c0daea15cf76a6fdbdb34927d07bd766
productteam102 <product.team102@gmail.com>
2025-06-27 23:51:55 +0700

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -40,6 +40,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head>
+        <link rel="icon" type="image/png" href="/favicon.png" />
+      </head>
       <body className={inter.className}>{children}</body>
     </html>
   )
