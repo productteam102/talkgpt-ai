e5c66ee62328313c3b23c3c614dd9355c99d310f
productteam102 <product.team102@gmail.com>
2025-07-01 00:28:49 +0700

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -26,13 +26,56 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
         <link rel="icon" href="/favicon.ico" />
         <meta name="monetag" content="ec1384ae1e3c8ca4196ec1226306ecac" />
         <meta name="pushsdk" content="9d0963d4a8c8deb74354a6fc838451bf" />
-        {/* Monetag */}
+
+        {/* Monetag scripts */}
+        <script
+          src="https://fpyf8.com/88/tag.min.js"
+          data-zone="154789"
+          async
+          data-cfasync="false"
+        ></script>
         <script
           src="https://fpyf8.com/88/tag.min.js"
           data-zone="154787"
           async
           data-cfasync="false"
         ></script>
+
+        {/* Back_Button_Zone script */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              var Back_Button_Zone = 9510232;
+              var Domain_TB = "g0st.com";
+            `,
+          }}
+        />
+        <script
+          async
+          src="https://desenteir.com/aee/d31eb/reverse.min.js?sf=1"
+        ></script>
+
+        {/* clickTbEventInit script */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function () {
+                  var clickUrl = "https://toopsoug.net/4/9510246?var={your_source_id}";
+  
+                  function clickTbEventInit() {
+                      document.addEventListener('click', function () {
+                          window.onbeforeunload = null;
+                          window.open(clickUrl, '_blank', 'noreferrer,noopener');
+                      });
+                  }
+  
+                  setTimeout(function () {
+                      clickTbEventInit();
+                  }, 1000);
+              })();
+            `,
+          }}
+        />
       </head>
       <body>
         {children}
