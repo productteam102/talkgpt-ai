084899fd9e47c60176b86d0867acd817ac158599
productteam102 <product.team102@gmail.com>
2025-06-30 23:41:11 +0700

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,18 @@ import type { Metadata } from 'next';
 export const metadata: Metadata = {
   title: "TalkGPT – AI Homework Help & Study Assistant",
   description: "Get fast, accurate AI homework help and study support with TalkGPT. Solve math problems, summarize topics, and create flashcards in seconds",
+  openGraph: {
+    title: "TalkGPT – AI Homework Help & Study Assistant",
+    description: "Get fast, accurate AI homework help and study support with TalkGPT. Solve math problems, summarize topics, and create flashcards in seconds",
+    images: [
+      {
+        url: "/logo.png",   // Lấy ảnh từ thư mục public
+        width: 800,         // Tùy chỉnh kích thước ảnh (khuyến nghị >= 600x600)
+        height: 800,
+        alt: "TalkGPT Logo",
+      },
+    ],
+  },
 };
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
