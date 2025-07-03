9f7eca7161852a6dbd28fc00216cc2a9e91bd9b7
productteam102 <product.team102@gmail.com>
2025-07-03 16:12:23 +0700

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -24,44 +24,8 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
     <html lang="en">
       <head>
         <link rel="icon" href="/favicon.ico" />
-        <meta name="pushsdk" content="9d0963d4a8c8deb74354a6fc838451bf" />
         <meta name="0a0b01ea9e34a49669e2490559e4d39b1d507db1" content="0a0b01ea9e34a49669e2490559e4d39b1d507db1" />
-
-        {/* Back_Button_Zone script */}
-        <script
-          dangerouslySetInnerHTML={{
-            __html: `
-              var Back_Button_Zone = 9510232;
-              var Domain_TB = "g0st.com";
-            `,
-          }}
-        />
-        <script
-          async
-          src="https://desenteir.com/aee/d31eb/reverse.min.js?sf=1"
-        ></script>
-
-        {/* clickTbEventInit script */}
-        <script
-          dangerouslySetInnerHTML={{
-            __html: `
-              (function () {
-                  var clickUrl = "https://toopsoug.net/4/9510246?var={your_source_id}";
-  
-                  function clickTbEventInit() {
-                      document.addEventListener('click', function () {
-                          window.onbeforeunload = null;
-                          window.open(clickUrl, '_blank', 'noreferrer,noopener');
-                      });
-                  }
-  
-                  setTimeout(function () {
-                      clickTbEventInit();
-                  }, 1000);
-              })();
-            `,
-          }}
-        />
+        
       </head>
       <body>
         {children}
