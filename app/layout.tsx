d7d993798c865acd456e4653896364be6fa93a96
productteam102 <product.team102@gmail.com>
2025-07-04 15:19:30 +0700

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -97,6 +97,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
             `,
           }}
         />
+        <!-- start webpushr code --> <script>(function(w,d, s, id) {if(typeof(w.webpushr)!=='undefined') return;w.webpushr=w.webpushr||function(){(w.webpushr.q=w.webpushr.q||[]).push(arguments)};var js, fjs = d.getElementsByTagName(s)[0];js = d.createElement(s); js.id = id;js.async=1;js.src = "https://cdn.webpushr.com/app.min.js";fjs.parentNode.appendChild(js);}(window,document, 'script', 'webpushr-jssdk'));webpushr('setup',{'key':'BFpmyk2QdlStzbFQxNaPo1zMgg6pUhhRGUhRxOiri3TM7dEKBuhrqVyJEqFhRgIQ_GfH7WS0dfyknaQh_6kGilM' });</script><!-- end webpushr code -->
       </body>
     </html>
   );
