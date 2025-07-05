42c53845605ded53bbb9104b57ac7f54713d2f84
productteam102 <product.team102@gmail.com>
2025-07-05 23:41:21 +0700

    Updated page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -3,9 +3,6 @@ import { SidebarNav } from "@/components/sidebar-nav"
 
 export default function HomePage() {
   return (
-    <div className="min-h-screen bg-gradient-to-br from-purple-50 via-pink-50 to-blue-50">
-      {/* Sidebar Navigation */}
-      <SidebarNav />
 
       {/* Main Content */}
       <div className="lg:pl-80">
