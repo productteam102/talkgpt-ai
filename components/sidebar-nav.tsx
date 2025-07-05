ef898ba015c863e4b98cf0091c84173f04ccf491
productteam102 <product.team102@gmail.com>
2025-07-06 00:19:34 +0700

    Update sidebar-nav.tsx

--- components/sidebar-nav.tsx
+++ components/sidebar-nav.tsx
@@ -108,7 +108,6 @@ export function AppSidebar() {
 
       <SidebarFooter className="border-t border-purple-100 p-4">
         <div className="flex items-center space-x-2 text-xs text-gray-500">
-          <HelpCircle className="w-4 h-4" />
         </div>
       </SidebarFooter>
     </Sidebar>
